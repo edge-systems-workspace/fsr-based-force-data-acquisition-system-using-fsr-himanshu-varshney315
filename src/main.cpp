e195940343cef9cabd30f33fb6a9701027704233
himanshu-varshney315 <himanshu.gupta_cs.aiml24@gla.acin>
2026-02-19 13:52:52 +0530

    update author information in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
  * and continuously reads sensor values, transmitting them via serial communication
  * at a baud rate of 9600.
  *
- * @author
+ * @author himanshu gupta
  * @date February 2026
  * @version 1.0
  */
